29ca3fa962abfceec2e247c4944b2bb57bbf6ea8
Syed Ali Hassan <syedalihassan6651@gmail.com>
2023-08-18 23:41:05 +0500

    Add dynamic sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,28 @@
+import { getProducts } from '@/lib/helpers';
+import { Product } from '@/lib/types';
+
+export default async function sitemap() {
+  const baseUrl = 'https://www.bejeweledbybt.com';
+  const categories = ['women', 'men', 'kids'];
+
+  const categoryUrls = categories.map(category => ({
+    url: `${baseUrl}/shop/${category}`,
+    lastModified: new Date(),
+  }));
+
+  const products = (await getProducts()) as Product[];
+  const productUrls = products.map(({ slug }) => ({
+    url: `${baseUrl}/product/${slug}`,
+    lastModified: new Date(),
+  }));
+
+  return [
+    {
+      url: '/',
+      lastModified: new Date(),
+    },
+
+    ...categoryUrls,
+    ...productUrls,
+  ];
+}
