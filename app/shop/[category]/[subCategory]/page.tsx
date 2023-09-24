2d760a98cc1b9bc94672efd19a5fb3c71fd6693c
Syed Ali Hassan <syedalihassan6651@gmail.com>
2023-09-24 17:36:10 +0500

    Fix bugs in active subCategory and filtering products based on subCategory.

--- app/shop/[category]/[subCategory]/page.tsx
+++ app/shop/[category]/[subCategory]/page.tsx
@@ -8,7 +8,7 @@ import Preloader from '@/components/Preloader';
 import ProductList from '@/components/ProductList';
 import { Product } from '@/lib/types';
 
-export const dynamicParams = false;
+// export const dynamicParams = false;
 export const revalidate = 2 * 3600;
 
 const categories = ['women', 'men', 'kids', 'all'];
@@ -36,7 +36,7 @@ type Props = { params: { category: string, subCategory: string } };
 const ShopCategory: NextPage<Props> = async ({ params }) => {
     const { category, subCategory } = params;
 
-    const products = (await getProducts({ category })) as Product[];
+    const products = (await getProducts({ category, subCategory })) as Product[];
     const subCategories = await getSubCategories(category, subCategory);
 
     return (
@@ -51,7 +51,7 @@ const ShopCategory: NextPage<Props> = async ({ params }) => {
 
                 {products.length ? <ProductList products={products} /> :
                     <div style={{ color: 'red', textAlign: 'center', display: 'flex', flexDirection: 'column', gap: '1.25rem', fontSize: '1.75rem' }}>
-                        <p><BiErrorCircle style={{ scale: 1.3, marginRight: '3px' }} /> No Products found for {category}!</p>
+                        <p><BiErrorCircle style={{ scale: 1.3, marginRight: '3px' }} /> No Products found for {category} in {subCategory}!</p>
 
                         <p style={{ color: 'var(--primary)' }}>Check out <Link style={{ display: 'inline', color: 'var(--primary)', textDecoration: 'underline' }} href='/shop'>All Products</Link>.</p> </div>}
             </div>
